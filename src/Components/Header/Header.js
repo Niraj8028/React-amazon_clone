845f91ef7eb52ba1749fd0939035f0a426bf356b
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-25 02:03:17 +0530

    navlinks added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,9 +8,34 @@ function Header() {
     <Link to='/'>
         <nav className='header'>
             <img className='header__logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='amazon-logo'/>
-            <div className='header__search'></div>
-            <input type="text" className='header__searchInput'/>
-            <SearchIcon className='header__searchIcon'/>
+
+            <div className='header__search'>
+                <input type="text" className='header__searchInput'/>
+                <SearchIcon className='header__searchIcon'/>
+            </div>
+                
+
+            <div className='header__navlinks'>
+                <Link to="/signin" className="header__link">
+                    <div className='header__options'>
+                        <label className='first_line'>Hello</label>
+                        <label className='second_line'>SignIn</label>
+                    </div>
+                </Link>
+                <Link to="/" className="header__link">
+                    <div className='header__options'>
+                        <label className='first_line'>Returns</label>
+                        <label className='second_line'>& Orders</label>
+                    </div>
+                </Link>
+                <Link to="/" className="header__link">
+                    <div className='header__options'>
+                        <label className='first_line'>Your</label>
+                        <label className='second_line'>Prime</label>
+                    </div>
+                </Link>
+            </div>
+
         </nav>
     </Link>
   )
