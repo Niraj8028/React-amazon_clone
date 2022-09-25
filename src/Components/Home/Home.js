94dca30d4a6906c8c7b8b641e0e36a8ee59c2e8f
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-26 01:33:54 +0530

    products added to homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,27 +7,96 @@ import './Home.css'
 function Home() {
   return (
     <div className='home'>
-        <img className='home-banner' alt='banner' src='https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg'/>\
-        <div className='product-rows'>
-          <Product/>
-          <Product/>
-          <Product/>
-          
-        </div>
+      <img className='home-banner' alt='banner' src='https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg' />\
+      <div className='product-rows'>
+        <Product
+          id="101"
+          title="PS5 Console- Horizon Forbidden West Bundle"
+          price={549.99
+          }
+          rating={5}
+          src="https://m.media-amazon.com/images/I/71fa5+U25cL._SX522_.jpg"
+        />
+        <Product
+          id="104"
+          title="PlayStation VR Marvel's Iron Man VR Bundle"
+          price={249.99
+          }
+          rating={5}
+          src="https://m.media-amazon.com/images/I/71hfAFNOO7L._AC_UY327_FMwebp_QL65_.jpg"
+        />
+        <Product
+          id="105"
+          title="NBA 2K23 - PlayStation 4"
+          price={59.99
+          }
+          rating={5}
+          src="https://m.media-amazon.com/images/I/81adYfJHzkL._AC_UY327_FMwebp_QL65_.jpg"
+        />
+        <Product
+          id="106"
+          title="Logitech G29 Gaming Racing Wheel"
+          price={89.99
+          }
+          rating={5}
+          src="https://m.media-amazon.com/images/I/61IYYoZ66VL._AC_UY327_FMwebp_QL65_.jpg"
+        />
         
-        <div className='product-rows'>
-          <Product/>
-          <Product/>
-          
-        </div>
-        <div className='product-rows'>
-          <Product/>
-          
-        </div>
+
+      </div>
+
+      <div className='product-rows'>
+      
+        <Product
+          id="102"
+          title="Meta Quest 2 — Advanced All-In-One Virtual Reality Headset — 128 GB"
+          price={399.99
+          }
+          rating={5}
+          src="https://m.media-amazon.com/images/I/61tE7IcuLmL._AC_UL480_QL65_.jpg"
+        />
+        <Product
+          id="103"
+          title="Xbox Core Wireless Controller – Carbon Black"
+          price={79.99
+          }
+          rating={5}
+          src="https://m.media-amazon.com/images/I/61X3uV04ztL._AC_UL480_QL65_.jpg"
+        />
+        <Product
+          id="103"
+          title="Horizon Forbidden West Collector's Edition - PS4 and PS5 Entitlements"
+          price={99.99
+          }
+          rating={5}
+          src="https://m.media-amazon.com/images/I/81jxpqvSJyL._AC_UY327_FMwebp_QL65_.jpg"
+        />
         
+      </div>
+      <div className='product-rows'>
+      
+      <Product
+          id="103"
+          title="DualShock 4 Wireless Controller for PlayStation 4 - Jet Black"
+          price={59.99
+          }
+          rating={5}
+          src="https://m.media-amazon.com/images/I/61IG46p-yHL._AC_UY327_FMwebp_QL65_.jpg"
+        />
+      <Product
+          id="103"
+          title="SteelSeries Arctis Nova Pro Wireless Xbox Multi-System Gaming Headset"
+          price={349.99
+          }
+          rating={5}
+          src="https://m.media-amazon.com/images/I/714i7dRiFoL._AC_UY327_FMwebp_QL65_.jpg"
+        />
+
+      </div>
+
     </div>
-    
-    
+
+
   )
 }
 
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,21 +2,22 @@ import React, { useContext } from 'react'
 import { UserContext } from '../../Context/UserContext'
 import "./Product.css"
 
-function Product({title,description,rating,price,src}) {
+function Product({title,id,rating,price,src}) {
     // const [{cart},dispatch]=useContext(UserContext);
     return (
         <div className='product'>
             <div className='product-info'>
-                <img className='image' alt='product-image' src='https://m.media-amazon.com/images/I/71fa5+U25cL._SX522_.jpg' />
+                <img className='image' alt='product-image' src={src} />
                 
-                    <p className='title'>PS5 Console- Horizon Forbidden West</p>
+                    <p className='title'>{title}</p>
                     <div className='rating'>
+                        <p>&#9733;</p>
                         <p>&#9733;</p>
                         <p>&#9733;</p>
                         <p>&#9733;</p>
                     </div>
 
-                    <p className='price'><strong>$</strong>549.99</p>
+                    <p className='price'><strong>$</strong>{price}</p>
                     <button className='add-to-cart'>
                         <p className='btn-text'>Add to Cart</p>
                     </button>
