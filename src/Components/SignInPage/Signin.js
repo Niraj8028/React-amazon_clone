9ff980bb5bb4649129101150e5efde7bf494cf6e
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-28 02:02:02 +0530

    login form done

--- src/Components/SignInPage/Signin.js
+++ src/Components/SignInPage/Signin.js
@@ -1,9 +1,24 @@
-import { Button } from '@mui/material'
-import React from 'react'
+
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Signin.css'
 
 function Signin() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    const login = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+    const register = (e) => {
+        e.preventDefault();
+        console.log(password);
+    }
+
+
+
     return (
         <div className='signin'>
             <Link to="/">
@@ -15,12 +30,18 @@ function Signin() {
                 <div className='signin__form'>
                     <form>
                         <h5>E-mail</h5>
-                        <input type="text"></input>
+                        <input value={email} onChange={(e) =>
+                                setEmail(e.target.value)
+                            } type="email"></input>
                         <h5>Password</h5>
-                        <input type="password"></input>
-                        <button className='signin_btn'>Sign in</button>
+                        <input value={password} onChange={e => setPassword(e.target.value)} type="password"></input>
+                        <button onClick={login}
+                             
+                            className='signin_btn'>Sign in</button>
+
                         <p>By signing-in you agree to Amazons Conditions of Use & Sale. Please see our Privacy Notice,.our cookies Notice and Interest-based Ads Notice</p>
-                        <button className='create_btn'>Create your Amazon Account</button>
+                        <button onClick={register}
+                          className='create_btn'>Create your Amazon Account</button>
                     </form>
                 </div>
             </div>
