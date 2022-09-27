e465d5f97981a2fac12f79ed8f3c65b855fa9cab
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-28 01:33:02 +0530

    login page styling done

--- src/Components/SignInPage/Signin.js
+++ src/Components/SignInPage/Signin.js
@@ -1,22 +1,26 @@
 import { Button } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Signin.css'
 
 function Signin() {
     return (
         <div className='signin'>
-            <img className='logo' src='https://thumbs.dreamstime.com/b/simple-vector-filled-flat-amazon-icon-logo-solid-black-pictogram-isolated-white-background-amazon-logo-159029074.jpg' />
+            <Link to="/">
+                <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' />
+            </Link>
+
             <div className='signin_container'>
-                <h1 className='title'>Sign In</h1>
+                <h1 className='tittle'>Sign In</h1>
                 <div className='signin__form'>
                     <form>
                         <h5>E-mail</h5>
                         <input type="text"></input>
                         <h5>Password</h5>
                         <input type="password"></input>
-                        <button>Sign in</button>
+                        <button className='signin_btn'>Sign in</button>
                         <p>By signing-in you agree to Amazons Conditions of Use & Sale. Please see our Privacy Notice,.our cookies Notice and Interest-based Ads Notice</p>
-                        <button>Create your Amazon Account</button>
+                        <button className='create_btn'>Create your Amazon Account</button>
                     </form>
                 </div>
             </div>
